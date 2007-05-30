6a833c0b67af949bbf53a4c3036ad243e1d2baa3
david <david@f03ff2f1-f02d-0410-970d-b9634babeaa1>
2007-05-30 16:16:43 +0000

    added first take of the virtual module from http://reductivelabs.com/trac/puppet/wiki/VirtualRecipe
    git-svn-id: http://club.black.co.at:82/svn/manifests/trunk@42 f03ff2f1-f02d-0410-970d-b9634babeaa1

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# virtual/init.pp -- miscellaneous stuff for virtual hosts and guests
+# Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
+# See LICENSE for the full license granted to you.
+# Based on the work of abnormaliti on http://reductivelabs.com/trac/puppet/wiki/VirtualRecipe
+# For the NTP stuff see ntp.pp
+
+file { "$rubysitedir/facter/virtual.rb":
+	source => "puppet://$servername/virtual/facter/virtual.rb",
+	mode => 0755, owner => root, group => root,
+}
