c9be6abb94520e09f020e2e33b69bdb4231fe798
david <david@f03ff2f1-f02d-0410-970d-b9634babeaa1>
2007-06-28 12:16:57 +0000

    basic vserver build support
    git-svn-id: http://club.black.co.at:82/svn/manifests/trunk@81 f03ff2f1-f02d-0410-970d-b9634babeaa1

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,3 +12,43 @@ file {
 		source => "puppet://$servername/virtual/facter/vserver.rb",
 		mode => 0755, owner => root, group => root;
 }
+
+class vserver_host {
+
+	package { [ 'util-vserver', debootstrap ]: ensure => installed, }
+
+	file {
+		"/usr/local/bin/build_vserver":
+			source => "puppet://$servername/virtual/build_vserver",
+			mode => 0755, owner => root, group => root,
+			require => [ Package['util-vserver'], Package[debootstrap] ];
+	}
+	
+}
+
+# ensure: present, stopped, running
+define vserver($ensure, $in_domain = $domain) {
+	case $ensure {
+		present: {
+			exec { "/usr/local/bin/build_vserver \"${name}\" \"${in_domain}\"":
+				creates => "/etc/vservers/${name}",
+				require => File["/usr/local/bin/build_vserver"],
+			}
+		}
+	}
+
+	case $ensure {
+		stopped: {
+			exec { "vserver ${name} stop":
+				onlyif => "test -e \$(readlink -f /etc/vservers/$name/run)",
+			}
+		}
+		running: {
+			exec { "vserver ${name} start":
+				unless => "test -e \$(readlink -f /etc/vservers/$name/run)",
+			}
+		}
+	}
+
+
+}
