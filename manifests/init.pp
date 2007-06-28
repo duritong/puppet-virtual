ae361ee4b4ac310c0d154f2e979738d44c09a956
david <david@f03ff2f1-f02d-0410-970d-b9634babeaa1>
2007-06-28 15:40:51 +0000

    added networking
    git-svn-id: http://club.black.co.at:82/svn/manifests/trunk@89 f03ff2f1-f02d-0410-970d-b9634babeaa1

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,6 +22,9 @@ class vserver_host {
 			source => "puppet://$servername/virtual/build_vserver",
 			mode => 0755, owner => root, group => root,
 			require => [ Package['util-vserver'], Package[debootstrap] ];
+		"/etc/vservers/local-interfaces/":
+			ensure => directory,
+			mode => 0755, owner => root, group => root;
 	}
 	
 }
@@ -37,10 +40,16 @@ define vs_create($in_domain) {
 
 # ensure: present, stopped, running
 define vserver($ensure, $in_domain = $domain) {
+	# TODO: wasn't there a syntax for using arrays as case selectors??
 	case $ensure {
 		present: { vs_create{$name: in_domain => $in_domain } }
 		running: { vs_create{$name: in_domain => $in_domain } }
 		stopped: { vs_create{$name: in_domain => $in_domain } }
+		default: { err("${fqdn}: vserver(${name}): unknown ensure '${ensure}'") }
+	}
+
+	file { "/etc/vservers/${name}/interfaces/":
+		ensure => directory, checksum => mtime,
 	}
 
 	case $ensure {
@@ -55,8 +64,54 @@ define vserver($ensure, $in_domain = $domain) {
 				unless => "test -e \$(readlink -f /etc/vservers/$name/run)",
 				require => Exec["vs_create_${name}"],
 			}
+
+			exec { "vserver ${name} restart":
+				refreshonly => true,
+				require => Exec["vs_create_${name}"],
+				alias => "vs_restart_${name}",
+				subscribe => File["/etc/vservers/${name}/interfaces/"]
+			}
 		}
 	}
 
+}
 
+define vs_interface($prefix = 24) {
+	file {
+		"/etc/vservers/local-interfaces/${name}/":
+			ensure => directory,
+			mode => 0755, owner => root, group => root;
+		"/etc/vservers/local-interfaces/${name}/ip":
+			content => "${name}\n",
+			mode => 0644, owner => root, group => root;
+		"/etc/vservers/local-interfaces/${name}/prefix":
+			content => "${prefix}\n",
+			mode => 0644, owner => root, group => root;
+		"/etc/vservers/local-interfaces/${name}/nodev":
+			ensure => present,
+			mode => 0644, owner => root, group => root;
+	}
+}
+
+define vs_ip($vserver, $ip, $ensure) {
+	case $ensure {
+		connected: {
+			file { "/etc/vservers/${vserver}/interfaces/${name}":
+				ensure => "/etc/vservers/local-interfaces/${ip}/",
+				require => File["/etc/vservers/local-interfaces/${ip}/"], 
+				notify => Exec["vs_restart_${vserver}"],
+			}
+		}
+		disconnected: {
+			file { "/etc/vservers/${vserver}/interfaces/${name}":
+				ensure => absent,
+				# TODO: fix message:
+				# warning: //ic/vs_ip[mailman_00]/File[/etc/vservers/mailman/interfaces/mailman_00]: Exec[vserver mailman restart] still depend on me -- not deleting
+				# notify => Exec["vs_restart_${vserver}"],
+			}
+		}
+		default: {
+			err( "${fqdn}: vs_ip: ${vserver} -> ${ip}: unknown ensure: '${ensure}'" )
+		}
+	}
 }
