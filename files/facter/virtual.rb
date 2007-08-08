c3f9d4de0399b4956a142165d02535cbe6b3d16d
david <david@f03ff2f1-f02d-0410-970d-b9634babeaa1>
2007-08-08 09:13:23 +0000

    fixed whitespace
    git-svn-id: http://club.black.co.at:82/svn/manifests/trunk@184 f03ff2f1-f02d-0410-970d-b9634babeaa1

--- files/facter/virtual.rb
+++ files/facter/virtual.rb
@@ -1,42 +1,39 @@
 # Copied from http://reductivelabs.com/trac/puppet/wiki/VirtualRecipe?version=6
 # Authored by abnormaliti with contributions by daniel@nsp.co.nz and mwr
-
+	
 # This defines the fact "virtual" with the possible values of "physical",
 # "vmware", "vmware-server", "xenu", or "xen0"
-
+	
 Facter.add("virtual") do
-  confine :kernel => :linux
-  
-  ENV["PATH"]="/bin:/sbin:/usr/bin:/usr/sbin"
-  
-  result = "physical"
-  
-  setcode do
-    
-    lspciexists = system "which lspci >&/dev/null"
-    if $?.exitstatus == 0
-      output = %x{lspci}
-      output.each {|p|
-        # --- look for the vmware video card to determine if it is virtual => vmware.
-        # ---     00:0f.0 VGA compatible controller: VMware Inc [VMware SVGA II] PCI Display Adapter
-        result = "vmware" if p =~ /VMware/
-      }
-    end
-
-    # VMware server 1.0.3 rpm places vmware-vmx in this place, other versions or platforms may not.
-    if FileTest.exists?("/usr/lib/vmware/bin/vmware-vmx")
-      result = "vmware_server"
-    end
-
-    if FileTest.exists?("/proc/sys/xen/independent_wallclock")
-      result = "xenu"
-    elsif FileTest.exists?("/proc/xen/capabilities")
-      txt = File.read("/proc/xen/capabilities")
-      if txt =~ /control_d/i
-        result = "xen0"
-      end
-    end
-    result
-  end
+	confine :kernel => :linux
+	
+	ENV["PATH"]="/bin:/sbin:/usr/bin:/usr/sbin"
+	
+	result = "physical"
+	
+	setcode do
+	
+		lspciexists = system "which lspci >&/dev/null"
+		if $?.exitstatus == 0
+			output = %x{lspci}
+			output.each {|p|
+				# --- look for the vmware video card to determine if it is virtual => vmware.
+				# ---     00:0f.0 VGA compatible controller: VMware Inc [VMware SVGA II] PCI Display Adapter
+				result = "vmware" if p =~ /VMware/
+			}
+		end
+		
+		# VMware server 1.0.3 rpm places vmware-vmx in this place, other versions or platforms may not.
+		if FileTest.exists?("/usr/lib/vmware/bin/vmware-vmx")
+			result = "vmware_server"
+		end
+		
+		if FileTest.exists?("/proc/xen/capabilities") and File.read("/proc/xen/capabilities") =~ /control_d/i
+			result = "xen0"
+		elsif FileTest.exists?("/proc/sys/xen/independent_wallclock")
+			result = "xenu"
+		end
+		result
+	end
 end
-
+	
