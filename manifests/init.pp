7826c2150857cec73d10d22e206d7d5ca827f22a
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2007-12-26 23:11:43 +0000

    added virtual module
    git-svn-id: https://svn/ipuppet/trunk/modules/virtual@323 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# virtual/init.pp -- miscellaneous stuff for virtual hosts and guests
+# Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
+# See LICENSE for the full license granted to you.
+# Based on the work of abnormaliti on http://reductivelabs.com/trac/puppet/wiki/VirtualRecipe
+
+modules_dir{ "virtual": }
+
+import "xen.pp"
---
+++ manifests/xen.pp
@@ -0,0 +1,40 @@
+# virtual/xen.pp -- XEN specifica
+# Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
+# See LICENSE for the full license granted to you.
+
+class munin::plugins::xen {
+	munin::remoteplugin {
+		xen_mem:	
+			source => "puppet://$servername/virtual/munin/xen_mem",
+			config => "user root";
+		xen_vm:
+			source => "puppet://$servername/virtual/munin/xen_vm",
+			config => "user root";
+	}
+}
+
+class xen::domain {
+	# install the special libc and parameters to enable it
+	$xen_ensure = $virtual ? {
+		'xen0' => present,
+		'xenu' => present,
+		default => 'absent'
+	}
+
+	case $ensure {
+		'absent': { err("xen::domain configured, but not detected") }
+	}
+
+	case $operatingsystem {
+		debian: { package { libc6-xen:
+				ensure => $xen_ensure,
+			  }
+			
+			  config_file {
+				"/etc/ld.so.conf.d/nosegneg.conf":
+				ensure => $xen_ensure,
+				content => "hwcap 0 nosegneg\n",
+			  }
+		}
+	}
+}
