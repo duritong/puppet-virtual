8278a93b396e54ce811661cad546f1cf83377cc0
David Schmitt <david@schmitt.edv-bus.at>
2008-04-06 16:08:14 +0200

    add more docs and fix syntax error

--- manifests/openvpn.pp
+++ manifests/openvpn.pp
@@ -4,6 +4,7 @@
 
 # configures the specified vserver for openvpn hosting
 # see also http://oldwiki.linux-vserver.org/some_hints_from_john
+# and http://linux-vserver.org/Frequently_Asked_Questions#Can_I_run_an_OpenVPN_Server_in_a_guest.3F
 
 class virtual::openvpn::host_base {
 	package { "openvpn": ensure => installed }
@@ -12,7 +13,7 @@ define virtual::openvpn::host() {
 	include virtual::openvpn::host_base
 	exec { "mktun for ${name}":
 		command => "./MAKEDEV tun",
-		cwd =< "/etc/vservers/${name}/vdir/dev",
+		cwd => "/etc/vservers/${name}/vdir/dev",
 		creates => "/etc/vservers/${name}/vdir/dev/net/tun";
 	}
 }
