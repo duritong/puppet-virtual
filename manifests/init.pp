c60622997d19b6db678a14ff61f634691331cac5
Marcel Haerry <haerry@puzzle.ch>
2008-04-22 12:18:14 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# virtual module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "virtual": }
+class virtual {}
