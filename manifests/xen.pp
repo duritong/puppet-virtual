700065663e8bb3002a408a1f75d5374b19aa94c5
Marcel Haerry <haerry@puzzle.ch>
2008-05-26 15:06:29 +0200

    added centos stuff, changed default behaviour
    Added packages which should be installed on centos hosts.
    Changed the default behaviour to not import by default the
    xen::domain class, as we don't want to turn a host to a xen
    host by puppet. This is done by some provisioning system like
    d-i or cobbler

--- manifests/xen.pp
+++ manifests/xen.pp
@@ -13,32 +13,29 @@ class munin::plugins::xen {
 	}
 }
 
-# always check whether xen stuff should be installed!
-include xen::domain
-
 class xen::domain {
     case $operatingsystem {
         debian: { include xen::domain::debian }
+        centos: { include xen::domain::centos }
         default: { include xen::domain::base }
     }
 }
 
 class xen::domain::base {} 
 
-class xen::domain::debian inherits xen::domain::base {
-	# install the special libc and parameters to enable it
-	$xen_ensure = $virtual ? {
-		'xen0' => present,
-		'xenu' => present,
-		default => 'absent'
-	}
+class xen::domain::centos inherits xen::domain::base {
+    package{ 'kernel-xen':
+        ensure => present,
+    }
+} 
 
+class xen::domain::debian inherits xen::domain::base {
 	# This package is i386 only
 	# See also http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=379444
 	case $architecture {
 		'i386': {
 			package { libc6-xen:
-				ensure => $xen_ensure,
+				ensure => 'present',
 			}
 		}
 	}
@@ -53,26 +50,24 @@ class xen::domain::debian inherits xen::domain::base {
 class xen::dom0 inherits xen::domain { 
     case $operatingsystem {
         debian: { include xen::dom0::debian }
+        centos: { include xen::dom0::centos }
         default: { include xen::dom0::base }
     }
 }
 
 class xen::dom0::base {}
+class xen::dom0::centos inherits xen::dom0::base {
+    package{ [ "xen", "xen-libs"]:
+        ensure => present,
+    }
+}
 class xen::dom0::debian inherits xen::dom0::base {
 	# install the packages required for managing xen
-	# TODO: this should be followed by a reboot
 	package { 
 		[ "xen-hypervisor-3.0.3-1-$architecture",
 		  "linux-image-xen-$architecture",
 		  'libsysfs2' 
 		]:
 			ensure => present
-	}
-
-	case $virtual {
-		'xen0': {}
-		default: {
-			err("dom0 support requested, but not detected. Perhaps you need to reboot ${fqdn}?")
-		}
 	}
 }
